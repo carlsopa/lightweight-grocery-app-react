20a5459ea13f8917caf3035e730ca287395e03bd
Paul R. Carlson <carlsonpar@gmail.com>
2020-02-05 22:27:52 -0500

    modified gitignore to include firebase init and modified main script to account for removed dependencies

--- src/tst.js
+++ src/tst.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import People from './components/test/people'
+//import People from './components/test/people'
 import Header from './components/test/grocery/top/header'
 import List from './components/test/grocery/list'
 import {GroceryDataProvider} from './context/test/DataContext'
